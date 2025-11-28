90b03622348c92987ed44fee22bb6dd23c303547
Anmol Tiwari <anmoltiwari8629927961@gmail.com>
2025-11-28 20:17:07 +0530

    update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -20,7 +20,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/school/contact", {
+      const res = await fetch("https://school-backend-4gsg.onrender.com/school/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
          body: JSON.stringify(form),
